5d8f10716d7ce7affd16fb6810db30446300bd54
Michael Becker <michael.f.becker@gmail.com>
2020-11-30 22:53:42 -0500

    Initial commit with sprint initializer files.

---
+++ src/test/java/com/cakefactory/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakefactory.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
